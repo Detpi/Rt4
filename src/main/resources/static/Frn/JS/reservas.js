75364ae4b7432b93ab7d0386cbcdad435313eccc
hei-ui <hr705885@gmail.com>
2021-10-28 11:53:46 -0500

    act

--- src/main/resources/static/Frn/JS/reservas.js
+++ src/main/resources/static/Frn/JS/reservas.js
@@ -1,6 +1,6 @@
 function traerInformacionReservacion(){
     $.ajax({
-        url:"http://129.151.112.171:8080/api/Reservation/all",
+        url:"http://129.151.112.171/api/Reservation/all",
         type:"GET",
         datatype:"JSON",
         success:function(respuesta){
@@ -18,8 +18,8 @@ function  pintarRespuestaReservacion(respuesta){
         myTable+="<td>"+respuesta[i].startDate+"</td>";
         myTable+="<td>"+respuesta[i].devolutionDate+"</td>";
         myTable+="<td>"+respuesta[i].status+"</td>";
-        myTable+="<td> <button onclick='borrarElemento("+respuesta[i].id+")'>Borrar</button>";
-        myTable+= '<td><button onclick="llamarInformacionCategorias('+respuesta[i].id+' )">Editar</button>';
+        myTable+="<td> <button onclick='borrarElementosReservacion("+respuesta[i].idReservation+")'>Borrar</button>";
+        myTable+= '<td><button onclick="llamarInformacionReservacion('+respuesta[i].idReservation+' )">Editar</button>';
         myTable+="</tr>";
     }
     myTable+="</table>";
@@ -27,35 +27,103 @@ function  pintarRespuestaReservacion(respuesta){
 }
 
 function guardarInformacionReservacion(){
-    let var4 = {
+    let myData={
+        
         startDate:$("#startDate").val(),
-        devolutionDate:$("#devolutionDated").val(),
+        devolutionDate:$("#devolutionDate").val(),
         status:$("#status").val()
-        };
-      
-        $.ajax({
-        type:'POST',
-        contentType: "application/json; charset=utf-8",
-        dataType: 'JSON',
-        data: JSON.stringify(var4),
-        
-        url:"http://129.151.112.171:8080/api/Reservation/save",
-       
-        
-        success:function(response) {
-                console.log(response);
-            console.log("Se guardo correctamente");
-            alert("Se guardo correctamente");
-            window.location.reload()
-    
-        },
-        
-        error: function(jqXHR, textStatus, errorThrown) {
-              window.location.reload()
-            alert("No se guardo correctamente");
-    
-    
-        }
-        });
+    };
 
+    let dataToSend=JSON.stringify(myData);
+        $.ajax({
+        url:"http://129.151.112.171/api/Reservation/save",
+        type:"POST",
+        data:dataToSend,
+        datatype:"JSON",
+        contentType: "application/json; charset=utf-8",
+        success:function(){
+            $("#resultado").empty();
+            $("#startDate").val("");
+            $("#devolutionDate").val("");
+            $("#status").val("");
+            traerInformacionReservacion();
+            alert("se ha guardado el dato");
+        }
+    });
 }
+function llamarInformacionReservacion (idReservation){
+	$.ajax({    
+		url : "http://129.151.112.171/api/Reservation/"+idReservation,
+		
+		type : 'GET',
+		dataType : 'json',
+		contentType: "application/json; charset=utf-8",
+  
+    success : function(respuesta) {
+		console.log(respuesta);
+        var item=respuesta;
+            $("#idReservation").val(item.idReservation);
+            $("#startDate").val(item.startDate);
+			$("#devolutionDate").val(item.devolutionDate);
+			$("#status").val(item.status);
+			
+        },
+      
+        error: function(jqXHR, textStatus, errorThrown) {
+              
+        }
+    });
+  
+  }
+function editarInformacionReservacion(){
+    let myData={
+        startDate:$("#startDate").val(),
+        devolutionDate:$("#devolutionDate").val(),
+        status:$("#status").val()
+        
+    
+    };
+    console.log(myData);
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"http://129.151.112.171/api/Reservation/update",
+        type:"PUT",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado5").empty();
+            $("#idReservation").val("");
+            $("#startDate").val("");
+            $("#devolutionDate").val("");
+            $("#status").val("");
+           
+            traerInformacionReservacion();
+            alert("se ha Actualizado")
+        }
+    });
+}
+function borrarElementosReservacion(idElemento){
+    let myData={
+        id:idElemento
+    };
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"http://129.151.112.171/api/Reservation/"+idElemento,
+        type:"DELETE",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            traerInformacionReservacion();
+            alert("Se ha Eliminado.")
+        }
+    });
+}
+
+
+            
+           
+        
+
